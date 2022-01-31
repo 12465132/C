e69e05f2663022f8f21563432531ae7780cc0650
12465132 <95362418+12465132@users.noreply.github.com>
2022-01-30 22:21:09 -0500

    test

---
+++ cpp/text2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+	
+//input
+int int1;
+printf("Function: \n"); //function display
+printf("Enter value: ");
+scanf("%d", &int1); 
+printf("Entered value: %d\n", int1); //value output
+printf("\n\n"); //output text
+//input
+
+	return 0;
+}
